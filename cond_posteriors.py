c5b8d5fe405f0a9faab132880ef78e3b659ddd97
ylf <iivodka5@gmail.com>
2023-11-24 18:29:28 +0200

    gl

---
+++ cond_posteriors.py
@@ -0,0 +1,22 @@
+def vbar(X):
+    raise NotImplementedError
+
+def R2q(Y, U, X, theta, z):
+    #hamza
+    raise NotImplementedError
+
+def phi(Y, U, X, z, beta, r2, q, sigma2):
+    #yvann
+    raise NotImplementedError
+
+def z(Y, U, X, phi, R2, q):
+    #yvann
+    raise NotImplementedError
+
+def sigma2(Y, U, X, phi, R2, q, z):
+    #rayane
+    raise NotImplementedError
+
+def betatilde(Y, U, X, phi, R2, q, sigma2, z):
+    #rayane
+    raise NotImplementedError
