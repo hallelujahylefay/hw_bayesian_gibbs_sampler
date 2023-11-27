86eab45df38b4e167532a254fb451334520a5e49
ylf <iivodka5@gmail.com>
2023-11-27 15:44:03 +0200

    wip

--- cond_posteriors.py
+++ cond_posteriors.py
@@ -1,7 +1,6 @@
 import numpy as np
-from scipy.stats import gamma 
+from scipy.stats import gamma
 from scipy.stats import multivariate_normal as mnormal
-
 
 k = 100
 T = 200
@@ -31,18 +30,19 @@ def Wtilde(Xtilde_v, sz_v, gamma2_v):
 def Xtilde(X, z):
     return X[:, z == 1]
 
-def sigma2_data(T_v,Ry_v,X): #le sigma_2 qui serviraà générer le jeu de données.
-    return (1/(Ry_v-1)) * sum((beta @ X.T)**2)/T_v
+
+def sigma2_data(beta_v, X, Ry):  # le sigma_2 qui serviraà générer le jeu de données.
+    return (1 / (Ry - 1)) * np.mean((beta_v @ X.T) ** 2, axis=0)
+
 
 def betahat(Wtildeinv_v, Xtilde_v, Y):
     return Wtildeinv_v @ Xtilde_v.T @ Y
 
 
-def R2q(X, z, n):
+def R2q(X, z):
     def posteriorR2q(q_v, R2_v, X, z, beta, sigma2_v):
         bz = beta @ np.diag(z) @ beta.T
         sz_v = sz(z)
-        sigma2_v= sigma2_data(T,Ry,X)
         return np.exp((-1 / (2 * sigma2_v)) * (k * vbar(X) * q_v * ((1 - R2_v) / R2_v) * bz)) * q_v ** (
                 3 / 2 * sz_v + a - 1) * (
                        1 - q_v) ** (k - sz_v + b - 1) * R2_v ** (A - 1 - sz_v / 2) * (1 - R2_v) ** (sz_v / 2 + B - 1)
@@ -123,18 +123,16 @@ def sigma2(Y, X, R2_v, q_v, z, gamma2):
     Wtilde_v = Wtilde(Xtilde_v, sz_v, gamma2_v)
     Wtildeinv_v = np.linalg.inv(Wtilde_v)
     betahat_v = betahat(Wtildeinv_v, Xtilde_v, Y)
-    #Lorsqu'on regroupera, toute cette initialisation de variables _v ne sera évidemment à faire qu'une fois.
-    return gamma(T/2,(Y.T @ Y - betahat_v.T @ (Xtilde_v.T @ Xtilde_v + np.eye(sz_v)/gamma2_v @ betahat_v)/2)) #Ytilde=Y
-    
+    # Lorsqu'on regroupera, toute cette initialisation de variables _v ne sera évidemment à faire qu'une fois.
+    return gamma(T / 2, (
+            Y.T @ Y - betahat_v.T @ (Xtilde_v.T @ Xtilde_v + np.eye(sz_v) / gamma2_v @ betahat_v) / 2))  # Ytilde=Y
 
 
 def betatilde(Y, X, R2_v, q_v, sigma2_v, z):
-    # rayane
     sz_v = sz(z)
     gamma2_v = gamma2(R2_v, q_v, X)
     Xtilde_v = Xtilde(X, z)
-    Xtilde_v=Xtilde(X,z)
-    mean=np.linalg.inv(np.eye(sz_v)/gamma2_v + Xtilde_v.T @ Xtilde_v ) @ Xtilde_v@Y #Pas de U*phi
-    cov=np.linalg.inv(np.eye(sz_v)/gamma2_v + Xtilde_v.T @ Xtilde_v )*sigma2_v
-    return mnormal(mean,cov)
-
+    id = np.eye(sz_v)
+    mean = np.linalg.inv(id / gamma2_v + Xtilde_v.T @ Xtilde_v) @ Xtilde_v @ Y  # Pas de U*phi
+    cov = np.linalg.inv(id / gamma2_v + Xtilde_v.T @ Xtilde_v) * sigma2_v
+    return mnormal(mean, cov)
