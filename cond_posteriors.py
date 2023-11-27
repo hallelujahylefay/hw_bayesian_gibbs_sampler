151fa634975b30591d5749481e93862a6a3e2bc7
RTalate <rayan.talate@ensae.fr>
2023-11-27 15:33:23 +0100

    Update cond_posteriors.py

--- cond_posteriors.py
+++ cond_posteriors.py
@@ -119,3 +119,39 @@ def betatilde(Y, X, R2_v, q_v, sigma2_v, z):
     cov=np.linalg.inv(np.eye(sz_v)/gamma2_v + Xtilde_v.T @ Xtilde_v )*sigma2_v
     return mnormal(mean,cov)
 
+#%% Génération du jeu de données
+
+rho=0.75
+"""
+J'imagine la syntaxe comme suit:
+s=5,10 ou 100
+Ry=2%,5% ou 50%
+X = ce que la prof demande
+
+beta_data=beta_data(s)
+epsilon_data=epsilon_data(Ry)
+On tire ce dataset 100 fois, on fait ce qu'on doit faire.
+
+On recommence avec
+s ou Ry qui change de valeur (ou les deux)
+Et ainsi de suite jusqu'à avoir couvert toutes les valeurs possibles du couple (s,Ry)
+
+"""    
+
+toeplitz_covariance_matrix = np.array([[rho ** abs(i - j) for i in range(k)] for j in range(k)])
+X = np.linalg.cholesky(toeplitz_covariance_matrix) @ np.random.normal(size=(k, T))
+X_t = np.array([X[i] for i in range(k)])
+
+def beta_data(s):
+    beta=np.random.normal(0,1,size=k)
+    zeroes_position=np.random.randint(0,k,size=s) # s nombres, choisis au hasard entre 0 et k. les coordonnées correspondantes dans beta seront rendues nulles.
+    beta[zeroes_position]=0
+    return beta
+    
+
+def epsilon(Ry,beta):
+    return np.random.normal(0,(Ry-1)^-1*sum(1/T*(beta.T@X_t[i])^2 for i in range()),size=T)    
+
+    
+def Y_data(X,beta_data,epsilon):
+    return np.array([X_t[i].T @ beta_data +epsilon[i] for i in range(T)])
