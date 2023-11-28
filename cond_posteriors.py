5e62d3404cd4d8eb93f8a8ed710df7ba4b3cf5e7
ylf <iivodka5@gmail.com>
2023-11-28 15:39:48 +0200

    opti

--- cond_posteriors.py
+++ cond_posteriors.py
@@ -2,8 +2,8 @@ import numpy as np
 from scipy.stats import gamma
 from scipy.stats import multivariate_normal as mnormal
 from scipy.special import gamma, gammaincc, comb
-from simulate_data import generate_dataset
 
+# from simulate_data import generate_dataset
 
 k = 100
 T = 200
@@ -12,6 +12,10 @@ a = 1
 b = 1
 A = 1
 B = 1
+
+grid_q = [i / 1000 for i in range(1, 100)] + [i / 100 for i in range(10, 90)] + [i / 1000 for i in range(900, 1000)]
+grid_R2 = [i / 1000 for i in range(1, 100)] + [i / 100 for i in range(10, 90)] + [i / 1000 for i in
+                                                                                  range(900, 1000)]
 
 
 def sz(z):
@@ -43,6 +47,8 @@ def betahat(Wtildeinv_v, Xtilde_v, Y):
 
 
 def R2q(X, z):
+    sz_v = sz(z)
+
     def joint_pdf(q_v, R2_v, X, z, beta_v, sigma2_v):
         bz = beta_v @ np.diag(z) @ beta_v.T
         sz_v = sz(z)
@@ -50,31 +56,25 @@ def R2q(X, z):
                 3 / 2 * sz_v + a - 1) * (
                        1 - q_v) ** (k - sz_v + b - 1) * R2_v ** (A - 1 - sz_v / 2) * (1 - R2_v) ** (sz_v / 2 + B - 1)
 
-    grid_q = [i / 1000 for i in range(1, 100)] + [i / 100 for i in range(10, 90)] + [i / 1000 for i in range(900, 1000)]
-    grid_R2 = [i / 1000 for i in range(1, 100)] + [i / 100 for i in range(10, 90)] + [i / 1000 for i in
-                                                                                      range(900, 1000)]
-
     def inc_gamma(a, x):
-        return gamma(a)*gammaincc(a, x)
-        
+        return gamma(a) * gammaincc(a, x)
+
     def univariate_pdf():
         # marginal of R, integrate joint posterior which is proportionate to a sum of lower 
         # incomplete gamma functions
         bz = beta_v @ np.diag(z) @ beta_v.T
-        sz_v = sz(z)
-        t= 1 / (2 * sigma2_v) * (k * vbar(X) * ((1 - R2_v) / R2_v) * bz)
-        return  sum([(-1) ** i * comb( k-sz_v , i ) * inc_gamma(3 * sz_v / 2 + i + 1, t ) for i in range(k-sz_v)])
+        t = 1 / (2 * sigma2_v) * (k * vbar(X) * ((1 - R2_v) / R2_v) * bz)
+        return sum([(-1) ** i * comb(k - sz_v, i) * inc_gamma(3 * sz_v / 2 + i + 1, t) for i in range(k - sz_v)])
 
-    def conditional_pdf(q_v,R2_v):
+    def conditional_pdf(q_v, R2_v):
         # distribution of q conditional on R2, proportionate to the joint posterior
         bz = beta_v @ np.diag(z) @ beta_v.T
-        sz_v = sz(z)
         return np.exp((-1 / (2 * sigma2_v)) * (k * vbar(X) * q_v * ((1 - R2_v) / R2_v) * bz)) * q_v ** (
-            3 / 2 * sz_v + a - 1) * (1 - q_v) ** (k - sz_v + b - 1) 
-    # initial values for q and R2
-    q_ = grid_q[np.random.random_integers(0,len(grid_q))]
-    R_ = grid_R2[np.random.random_integers(0,len(grid_R2))]
+                3 / 2 * sz_v + a - 1) * (1 - q_v) ** (k - sz_v + b - 1)
+        # initial values for q and R2
 
+    q_ = grid_q[np.random.random_integers(0, len(grid_q))]
+    R_ = grid_R2[np.random.random_integers(0, len(grid_R2))]
 
     def invCDF(pdf, grid, u):
         weights = [pdf(i) for i in grid]
@@ -84,13 +84,15 @@ def R2q(X, z):
         return grid[np.argmax(cdf > u)]
 
     def sampleqR():
-        u=np.random.uniform(0,1)
-        R_=invCDF(univariate_pdf(),grid_R2,u)
+        u = np.random.uniform(0, 1)
+        R_ = invCDF(univariate_pdf(), grid_R2, u)
+
         def pdf_q(q_v):
-            return conditional_pdf(q_v,R_)
-        v=np.random.uniform(0,1)
-        q_=invCDF(pdf_q(),grid_q,v))
-        return (q_,R_)
+            return conditional_pdf(q_v, R_)
+
+        v = np.random.uniform(0, 1)
+        q_ = invCDF(pdf_q, grid_q, v)
+        return q_, R_
 
     return sampleqR  # function that will be looped over to generate samples of (q, R) given X z
 
@@ -125,7 +127,7 @@ def z(Y, X, R2_v, q_v):
                 z[i] = 1
         return z
 
-    def gibbs(init, iter):
+    def gibbs(init, iter=1):
         z = init
         for i in range(iter):
             z = iter_gibbs(z)
@@ -151,9 +153,9 @@ def betatilde(Y, X, R2_v, q_v, sigma2_v, z):
     gamma2_v = gamma2(R2_v, q_v, X)
     Xtilde_v = Xtilde(X, z)
     id = np.eye(sz_v)
-    mean = np.linalg.inv(id / gamma2_v + Xtilde_v.T @ Xtilde_v) @ Xtilde_v @ Y  # Pas de U*phi
-    cov = np.linalg.inv(id / gamma2_v + Xtilde_v.T @ Xtilde_v) * sigma2_v
+    invTerm = np.linalg.inv(id / gamma2_v + Xtilde_v.T @ Xtilde_v)
+    mean = invTerm @ Xtilde_v @ Y  # Pas de U*phi
+    cov = invTerm * sigma2_v
     return mnormal(mean, cov)
 
-
-#%% 
+# %%
