0d8c50d4a8aba1a369183669184b9a597d72b352
ylf <iivodka5@gmail.com>
2023-12-04 23:51:24 +0200

    ...

--- gibbs.py
+++ gibbs.py
@@ -1,5 +1,4 @@
 import numpy as np
-from tqdm import tqdm
 import cond_posteriors as cp
 
 k = 100
@@ -13,15 +12,15 @@ def gibbs_per_block(X, Y, init, ITERATION=200, BURNIN_period=100):
     beta_tilde_vs = np.zeros((ITERATION, k))
 
     z_v, beta_v, sigma2_v, q_v = init
-    for n in tqdm(range(ITERATION + BURNIN_period)):
+    for n in range(ITERATION + BURNIN_period):
         R2_v, q_v = cp.R2q(X, z_v, beta_v, sigma2_v)()
         z_v = cp.z(Y, X, R2_v, q_v)(z_v)
         sigma2_v = cp.sigma2(Y, X, R2_v, q_v, z_v)
-        beta_v_tilde = cp.betatilde(Y, X, R2_v, q_v, sigma2_v, z_v)
+        beta_v = cp.betatilde(Y, X, R2_v, q_v, sigma2_v, z_v)
         if n >= BURNIN_period:
             R2_vs[n - BURNIN_period] = R2_v
             q_vs[n - BURNIN_period] = q_v
             z_vs[n - BURNIN_period] = z_v
             sigma2_vs[n - BURNIN_period] = sigma2_v
-            beta_tilde_vs[n - BURNIN_period] = beta_v_tilde
-    return R2_vs, q_vs, z_vs, sigma2_vs, beta_v_tilde
+            beta_tilde_vs[n - BURNIN_period] = beta_v
+    return R2_vs, q_vs, z_vs, sigma2_vs, beta_v
