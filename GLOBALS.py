1ce4cc1fc58b51c51017a4ae26005af24a34beed
ylf <iivodka5@gmail.com>
2023-12-06 03:51:04 +0200

    ...

--- GLOBALS.py
+++ GLOBALS.py
@@ -13,8 +13,8 @@ grid += [i for i in np.arange(0.11, 0.91, 0.01)]
 grid += [i for i in np.arange(0.901, 1, 0.001)]
 grid = np.array(grid, dtype=np.float64)
 
-surface = np.zeros(len(grid), dtype=np.float64)
-surface[:-1] = np.sum(
-    np.array([[(grid[i] - grid[i - 1]) * (grid[j] - grid[j - 1]) for i in range(1, len(grid))] for j in
-              range(1, len(grid))], dtype=np.float64), axis=1)
-surface[-1] = surface[-2]
+ds = np.array([[(grid[i] - grid[i - 1]) * (grid[j] - grid[j - 1]) for i in range(1, len(grid))] for j in
+               range(1, len(grid))], dtype=np.float64)
+dx = np.sum(ds, axis=1)
+grid = grid[:-1]
+
--- cond_posteriors.py
+++ cond_posteriors.py
@@ -27,6 +27,49 @@ def betahat(Wtilde_v, Xtilde_v, Y):
 
 def R2q(X, z, beta_v, sigma2_v):
     sz_v = sz(z)
+    bz = beta_v @ np.diag(z) @ beta_v.T
+    vbarX_v = vbar(X)
+
+    def joint_pdf(q_v, R2_v):
+        return np.exp((-bz / (2 * sigma2_v * gamma2(R2_v, q_v, vbarX_v)))) * q_v ** (
+                3 / 2 * sz_v + a - 1) * (
+                       1 - q_v) ** (k - sz_v + b - 1) * R2_v ** (A - 1 - sz_v / 2) * (1 - R2_v) ** (sz_v / 2 + B - 1)
+
+    @np.vectorize
+    def univariate_pdf(q_v):
+        # marginal of q, integrate joint posterior
+        _univariate_pdf = lambda R2_v: joint_pdf(q_v, R2_v)
+        return np.sum(_univariate_pdf(grid) * dx)
+
+    def cdf(pdf):
+        weights = pdf(grid) * dx
+        cdf = np.cumsum(weights)
+        cdf /= cdf[-1]
+        return cdf
+
+    def invCDF(cdf, u):
+        a = np.where(cdf < u)[0]
+        if len(a) == 0:
+            return grid[-1]
+        return grid[a[-1]]
+
+    cdfq = cdf(univariate_pdf)
+
+    def sampleqR():
+        u = np.random.uniform(0, 1)
+        q = invCDF(cdfq, u)
+
+        cdfR2conditiononq = cdf(lambda R2: joint_pdf(q, R2))
+        v = np.random.uniform(0, 1)
+        R2 = invCDF(cdfR2conditiononq, v)
+        return R2, q
+
+    return sampleqR()  # function that will be looped over to generate samples of (q, R) given X z
+
+
+"""
+def R2q(X, z, beta_v, sigma2_v):
+    sz_v = sz(z)
     bz = beta_v.T @ np.diag(z) @ beta_v
     vbarX_v = vbar(X)
 
@@ -38,25 +81,24 @@ def R2q(X, z, beta_v, sigma2_v):
     @np.vectorize
     def univariate_pdf(q_v):
         # marginal of q, integrate joint posterior
-        @np.vectorize
         def _log_univariate_pdf(R2_v):
             return log_joint_pdf(q_v, R2_v)
 
-        log_univariate_pdf = _log_univariate_pdf(grid) + np.log(surface)
+        log_univariate_pdf = _log_univariate_pdf(grid) + np.log(dx)
         log_univariate_pdf -= np.max(log_univariate_pdf)
         return np.sum(np.exp(log_univariate_pdf))
 
     def cdf(pdf_or_logpdf, logpdf=False):
         if logpdf:
             logpdf = pdf_or_logpdf
-            logweights = logpdf(grid) + np.log(surface)
+            logweights = logpdf(grid) + np.log(dx)
             logweights -= np.max(logweights)
             weights = np.exp(logweights)
             cdf = np.cumsum(weights)
             cdf /= cdf[-1]
             return cdf
         pdf = pdf_or_logpdf
-        weights = pdf(grid) * surface
+        weights = pdf(grid) * dx
         cdf = np.cumsum(weights)
         cdf /= cdf[-1]
         return cdf
@@ -79,6 +121,7 @@ def R2q(X, z, beta_v, sigma2_v):
         return R2, q
 
     return sampleqR()  # function that will be looped over to generate samples of (q, R) given X z
+"""
 
 
 def z(Y, X, R2_v, q_v, z_v):
