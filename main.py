65ba893f9e4af50789afe26c27dad9e8a670a16e
aitbiz <107467274+aitbiz@users.noreply.github.com>
2024-01-15 21:40:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,31 @@
-import pickle
-from gibbs import gibbs_per_block
-from simulate_data import generate_dataset, initialize_parameters
-from multiprocessing import Process
+import pandas as pd
+import numpy as np
+from gibbs_ps import gibbs_per_block
+from sklearn.linear_model import Lasso
 
-s_list = [5]
-Ry_list = [0.5]
-no_datasets = 1
+df=pd.read_csv(path)
 
-output_path = "./"
-multiprocess = False
-no_multi = 2
+remove= ['sasdate','date','year','Prev_Indpro','Industial monthly growth rate','INDPRO']
+regressors=[ i for i in df.columns if i not in remove]
+Y=np.array(df['INDPRO'])
+X=np.array(df[regressors])
+U=np.ones(len(df))
+
+lasso_reg = Lasso(alpha=0.1, fit_intercept=False)
+lasso_reg.fit(np.c_[U, X], Y)
+beta = lasso_reg.coef_[1:]
+phi = lasso_reg.coef_[0]
+y_pred = lasso_reg.predict(np.c_[U, X])
+residuals = Y - y_pred
+sigma2 = np.var(residuals)
+z = np.where(beta != 0, True, False)
+q = np.sum(z) / len(z)
+z, beta, phi, sigma2, q
+
+
 
 BURNIN_period = 1000
 ITERATION = 5000
 
-
-def run(dataset, identifier, output_path="."):
-    for (s, Ry) in dataset.keys():
-        X, Y, beta_v, z_v, sigma2_v, q_v = dataset[(s, Ry)]
-        init = initialize_parameters(X, Y)
-        print(f"(i, s, Ry): {identifier, s, Ry}, sigma²: {init[2]}, q: {init[3]}")
-        res_gibbs = X, Y, gibbs_per_block(X, Y, init, ITERATION=ITERATION, BURNIN_period=BURNIN_period)
-        with open(f'{output_path}/{identifier}_{s}_{Ry}.pickle', 'wb') as handle:
-            pickle.dump(res_gibbs, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-if __name__ == "__main__":
-    datasets = generate_dataset(s_list, Ry_list, no_datasets)
-    if multiprocess:
-        for j in range(no_datasets//no_multi):
-            procs = []
-            for i in range(no_multi * j, no_multi * (j+1)):
-                procs.append(Process(target=run, args=(datasets[i], i, output_path)))
-            for proc in procs:
-                proc.start()
-            for proc in procs:
-                proc.join()
-    else:
-        for i in datasets.keys():
-            run(datasets[i], i, output_path)
+init=z, beta, phi, sigma2, q
+res_gibbs = X, Y, gibbs_per_block(X, Y, U, init, ITERATION=ITERATION, BURNIN_period=BURNIN_period)
